a5abae259ea83df0190280272a6d6c8ae6595bae
juanma <juanmanuel.vaca@cenits.es>
2015-04-01 11:09:53 +0200

    Estado inicial

--- src/Cupon/TiendaBundle/Controller/ExtranetController.php
+++ src/Cupon/TiendaBundle/Controller/ExtranetController.php
@@ -14,17 +14,28 @@ class ExtranetController extends Controller
 		return $this->render('TiendaBundle:Extranet:temporal.html.twig', array());
     }
 	
-	public function loginAction()
-	{
-		$peticion = $this->getRequest();
-		$sesion = $peticion->getSession();
-		$error = $peticion->attributes->get(
-		SecurityContext::AUTHENTICATION_ERROR,
-		$sesion->get(SecurityContext::AUTHENTICATION_ERROR)
-		);
+    public function loginAction()
+    {
+	$peticion = $this->getRequest();
+	$sesion = $peticion->getSession();
+	$error = $peticion->attributes->get(
+	SecurityContext::AUTHENTICATION_ERROR,
+	$sesion->get(SecurityContext::AUTHENTICATION_ERROR)
+	);
 
-		return $this->render('TiendaBundle:Extranet:login.html.twig', array(
+	return $this->render('TiendaBundle:Extranet:login.html.twig', array(
 		'error' => $error
-		));
-	}
+	));
+    }
+    
+    public function ofertaEditarAction($id)
+    {
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->find($id);
+        if (false === $this->get('security.context')->isGranted(
+            'ROLE_EDITAR_OFERTA', $oferta
+        )) {
+            throw new AccessDeniedException();
+        }
+        // ...
+    }
 }
